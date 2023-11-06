1487d4933295719020788f3c9ec9ed8fbc86f575
mehrnaz98 <mehrnaz.eft98@gmail.com>
2023-11-06 22:39:11 +0330

    Test for calculateTotalPayableAmount added

--- spec/scriptSpec.js
+++ spec/scriptSpec.js
@@ -9,3 +9,8 @@ it("Calculate Simple Interest", () => {
   var actual = calculateSimpleInterest(1000, 5, 1);
   expect(actual).toBe(50);
 });
+
+it("Calculate Total Interest", () => {
+  var actual = calculateTotalPayableAmount(1000, 50);
+  expect(actual).toBe(1050);
+});
