4bc5112543b4aeb45594dae56e634d0e7d51f464
mehrnaz98 <mehrnaz.eft98@gmail.com>
2023-11-06 22:57:03 +0330

    test for calculateSimpleInterest and calculateTotalPayableAmount by Jasmine

--- spec/scriptSpec.js
+++ spec/scriptSpec.js
@@ -3,14 +3,14 @@ const {
   calculateTotalPayableAmount,
 } = require("../script");
 
-describe("Interest Rate Calculator Tests", () => {});
+describe("Interest Rate Calculator Tests", () => {
+  it("Calculate Simple Interest", () => {
+    var actual = calculateSimpleInterest(1000, 5, 1);
+    expect(actual).toBe(50);
+  });
 
-it("Calculate Simple Interest", () => {
-  var actual = calculateSimpleInterest(1000, 5, 1);
-  expect(actual).toBe(50);
-});
-
-it("Calculate Total Interest", () => {
-  var actual = calculateTotalPayableAmount(1000, 50);
-  expect(actual).toBe(1050);
+  it("Calculate Total Interest", () => {
+    var actual = calculateTotalPayableAmount(1000, 50);
+    expect(actual).toBe(1050);
+  });
 });
