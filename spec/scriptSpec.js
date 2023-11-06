d366dd0250a313d56db03f5224dc8f6e5c033000
mehrnaz98 <mehrnaz.eft98@gmail.com>
2023-11-06 22:38:33 +0330

    Test for calculateSimpleInterest added

--- spec/scriptSpec.js
+++ spec/scriptSpec.js
@@ -4,3 +4,8 @@ const {
 } = require("../script");
 
 describe("Interest Rate Calculator Tests", () => {});
+
+it("Calculate Simple Interest", () => {
+  var actual = calculateSimpleInterest(1000, 5, 1);
+  expect(actual).toBe(50);
+});
