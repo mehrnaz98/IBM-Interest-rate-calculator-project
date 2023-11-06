ffc9ca8b70d95545c9d2724e3e71489ec91f0f60
mehrnaz98 <mehrnaz.eft98@gmail.com>
2023-11-06 22:16:21 +0330

    Files added

---
+++ script.js
@@ -0,0 +1,13 @@
+const calculate = () => {
+  let p = Number(document.getElementById("principal").value);
+  let r = Number(document.getElementById("rate").value);
+  let t = Number(document.getElementById("time").value);
+  let simpleInterest = (p * r * t) / 100;
+  let amount = p - simpleInterest;
+
+  let result = document.getElementById("result");
+
+  result.innerHTML = `<div>Principal Amount: <span>${p.toFixed(2)}</span></div>
+  <div>Total Interest: <span>${simpleInterest.toFixed(2)}</span></div>
+  <div>Total Amount: <span>${amount.toFixed(2)}</span></div>`;
+};
